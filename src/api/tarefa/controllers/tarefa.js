a14663a0228a5e4f6cc4fcaf28ee64b3d13597b6
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2023-02-01 21:59:07 +0000

    Docker

--- src/api/tarefa/controllers/tarefa.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tarefa controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::tarefa.tarefa');
--- src/api/tarefa/services/tarefa.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * tarefa service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::tarefa.tarefa');
